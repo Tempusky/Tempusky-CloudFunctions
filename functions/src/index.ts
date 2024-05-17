dc2970a0afcd98982b261d22761f560deda98fa3
Pere <peremunozfiguerol33@gmail.com>
2024-05-18 00:59:16 +0200

    Implement function to write the location and location coordinates from the user coordinates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,83 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import axios from "axios";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const writeLocationFromCoordinates = functions.firestore
+  .document("environment_sensors_data/{documentId}")
+  .onCreate(async (snapshot, context) => {
+    const documentId = context.params.documentId;
+    const data = snapshot.data();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const geoPoint = data.sync_cords;
+
+    const longitude = geoPoint.longitude;
+    const latitude = geoPoint.latitude;
+
+    // Check if longitude and latitude are valid
+    if (longitude == null || latitude == null) {
+      console.error("Longitude or Latitude is missing in the document");
+      return null;
+    }
+
+    try {
+      // Make an API call to get the location from the coordinates
+      const response = await axios.get(
+        "https://api.bigdatacloud.net/data/reverse-geocode-client",
+        {
+          params: {
+            latitude,
+            longitude,
+          },
+        }
+      );
+
+      const locationData = response.data;
+
+      // Check if the response contains location data
+      if (!locationData || !locationData.locality) {
+        console.error("No location data found in the response");
+        return null;
+      }
+
+      // Extract the location string
+      let location = "unknown";
+      if (locationData.city) {
+        location = locationData.city;
+      } else {
+        location = locationData.locality;
+      }
+
+
+      const cordResponse = await axios.get(
+        "https://geocoding-api.open-meteo.com/v1/search",
+        {
+          params: {
+            name: location,
+          },
+        }
+      );
+
+      const cordData = cordResponse.data;
+      let locationLongitude = longitude;
+      let locationLatitude = latitude;
+      if (cordData.results.length > 0) {
+        locationLongitude = cordData.results[0].longitude;
+        locationLatitude = cordData.results[0].latitude;
+      }
+
+      const newGeoPoint = new admin.firestore.GeoPoint(
+        locationLatitude, locationLongitude);
+
+      // Write the location data to the document
+      await snapshot.ref.set({location, location_cords: newGeoPoint},
+        {merge: true});
+
+      console.log(`Location data written for document ${documentId}`);
+    } catch (error) {
+      console.error("Error fetching location data", error);
+    }
+
+    return null;
+  });
